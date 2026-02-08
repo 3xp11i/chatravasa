1c8e33219247285b048ebafeaf47c76758631e4d
prakhartiwari0 <65062036+prakhartiwari0@users.noreply.github.com>
2026-02-08 19:39:52 +0530

    fix: move settings button to bottom of sidebar

--- app/components/sideBar.vue
+++ app/components/sideBar.vue
@@ -35,12 +35,6 @@
                     </svg>
                     <span>{{ t('editProfile') }}</span>
                 </NuxtLink>
-                <NuxtLink to="/settings" @click="close" class="sidebar-link">
-                    <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-                        <path fill-rule="evenodd" d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z" clip-rule="evenodd" />
-                    </svg>
-                    <span>{{ t('settings') }}</span>
-                </NuxtLink>
                 <NuxtLink v-if="isAdmin" to="/dashboard" @click="close" class="sidebar-link">
                     <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
                         <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
@@ -64,7 +58,18 @@
                     <option value="en">{{ t('english') }}</option>
                     <option value="hi">{{ t('hindi') }}</option>
                 </select>
-
+                <NuxtLink to="/settings"
+                          @click="close"
+                          class="sidebar-link">
+                    <svg class="w-5 h-5"
+                         fill="currentColor"
+                         viewBox="0 0 20 20">
+                        <path fill-rule="evenodd"
+                              d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z"
+                              clip-rule="evenodd" />
+                    </svg>
+                    <span>{{ t('settings') }}</span>
+                </NuxtLink>
 
                 <NuxtLink to="/contact" @click="close" class="sidebar-link">
                     <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
